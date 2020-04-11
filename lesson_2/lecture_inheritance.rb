cd8d0dca4f02fefbcaa05ba22ae2cc2ddded8196
Jessica Davey <jessica@jessicadavey.com>
2020-04-11 18:32:51 -0500

    finished lecture: inheritance

--- lesson_2/lecture_inheritance.rb
+++ lesson_2/lecture_inheritance.rb
@@ -1,22 +1,22 @@
 # 1. Given this class, create a sub-class from Dog called Bulldog 
 # overriding the swim method to return "can't swim!"
 
-# class Dog
-#   def speak
-#     'bark!'
-#   end
+class Dog
+  def speak
+    'bark!'
+  end
 
-#   def swim
-#     'swimming!'
-#   end
-# end
+  def swim
+    'swimming!'
+  end
+end
 
-# class Bulldog < Dog
+class Bulldog < Dog
 
-#   def swim
-#     "can't swim!"
-#   end
-# end
+  def swim
+    "can't swim!"
+  end
+end
 
 # 2. Create a new class called Cat, which can do everything a dog can, except swim or fetch. 
 # Assume the methods do the exact same thing. 
@@ -59,3 +59,10 @@ chloe = Cat.new
 puts chloe.speak
 puts chloe.run
 puts chloe.jump
+
+
+# 4. What is the method lookup path and how is it important?
+
+# The method lookup path is the order in which classes are inspected to find methods
+# when a method is called.  If the same method is found in multiple classes, the one
+# found first will be used.
