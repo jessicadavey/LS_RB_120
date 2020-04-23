883141f808c5b10812462457dc6383361663eb37
jessicadavey <jessica@jessicadavey.com>
2020-04-23 17:48:37 +0000

    refactored lizard and spock

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Score
   WIN_SCORE = 3
 
@@ -33,51 +35,30 @@ class Score
 end
 
 class Move
+  BEATS = { 'rock' => %w(scissors lizard),
+            'paper' => %w(rock spock),
+            'scissors' => %w(lizard paper),
+            'lizard' => %w(paper spock),
+            'spock' => %w(rock scissors) }
+
   VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
   def initialize(value)
     @value = value
   end
 
-  def scissors?
-    @value == 'scissors'
-  end
-
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-  
-  def lizard?
-    @value == 'lizard'
-  end
-  
-  def spock?
-    @value == 'spock'
-  end
-
   def >(other_move)
-    (rock? && (other_move.scissors? || other_move.lizard?)) ||
-      (paper? && (other_move.rock? || other_move.spock?)) ||
-      (scissors? && (other_move.paper? || other_move.lizard?)) ||
-      (lizard? && (other_move.paper? || other_move.spock?)) ||
-      (spock? && (other_move.rock? || other_move.scissors?))
+    BEATS[@value].include?(other_move.to_s)
   end
 
   def <(other_move)
-    (rock? && (other_move.paper? || other_move.spock?)) ||
-      (paper? && (other_move.scissors? || other_move.lizard?)) ||
-      (scissors? && (other_move.rock? || other_move.spock?)) ||
-      (lizard? && (other_move.scissors? || other_move.rock?)) ||
-      (spock? && (other_move.paper? || other_move.lizard?))
+    !BEATS[@value].include?(other_move.to_s) && @value != other_move.to_s
   end
 
   def to_s
     @value
   end
+
 end
 
 class Player
